68608b8628a8e19ecca8ef63a8126dc6e46eab89
Mark Dangal <pratish5858@gmail.com>
2025-03-09 18:03:57 +0545

    fixed point code

--- OpenGl/Points.cpp
+++ OpenGl/Points.cpp
@@ -1,44 +1,35 @@
 #include <GL/glut.h>
 
-void display();
-void myInit();
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1, 0, 0); // Red points
+    glPointSize(10);    // Bigger points for visibility
+
+    glBegin(GL_POINTS);
+    glVertex2f(0.0, 0.0);   // Center point
+    glVertex2f(-0.5, -0.5); // Bottom-left
+    glVertex2f(0.5, 0.5);   // Top-right
+    glVertex2f(0.S5, -0.5);  // Bottom-right
+    glVertex2f(-0.5, 0.5);  // Top-left
+    glEnd();
+
+    glFlush();
+}
+
+void init()
+{
+    glClearColor(1, 1, 1, 1); // White background
+    gluOrtho2D(-1, 1, -1, 1); // 2D coordinate system
+}
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Points");
-	glutDisplayFunc(display);
-
-	myInit();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Bigger Points");
+    init();
+    glutDisplayFunc(display);
     glutMainLoop();
-	return 0;
 }
-
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
-
-	glBegin(GL_POINTS);
-	glVertex2f(10.0, 10.0);
-	glVertex2f(150.0, 80.0);
-	glVertex2f(100.0, 20.0);
-	glVertex2f(200.0, 100.0);
-
-	glEnd();
-	glFlush();
-}
-
-void myInit()
-{
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(4.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
-}
-
