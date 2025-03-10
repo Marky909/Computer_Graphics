945530e3edc9238c4388837fd49b5ea3cfc3c8c5
Mark Dangal <pratish5858@gmail.com>
2025-03-10 08:01:49 +0545

    change

--- other_graphics_H/Depth_sort.cpp
+++ other_graphics_H/Depth_sort.cpp
@@ -6,57 +6,43 @@ int main() {
     initgraph(&gd, &gm, (char*)"");
 
     int boundry[4][3] = {{50, 50, 50}, {500, 50, 50}, {50, 500, 50}, {500, 500, 50}};
-    int rect1[4][3] = {{80, 80, 40}, {80, 100, 40}, {300, 80, 40}, {300, 300, 40}};
-    int rect2[4][3] = {{200, 70, 30}, {400, 70, 30}, {200, 200, 30}, {400, 400, 30}};
+    int rect1[4][3] = {{80, 80, 40}, {80, 300, 40}, {300, 80, 40}, {300, 300, 40}};
+    int rect2[4][3] = {{200, 70, 30}, {200, 200, 30}, {400, 70, 30}, {400, 400, 30}};
 
+    // Draw boundary in white
+    setcolor(WHITE);
     rectangle(boundry[0][0], boundry[0][1], boundry[3][0], boundry[3][1]);
-    rectangle(rect1[0][0], rect1[0][1], rect1[3][0], rect1[3][1]);
-    rectangle(rect2[0][0], rect2[0][1], rect2[3][0], rect2[3][1]);
 
-    int a = boundry[0][2];
-    int b = rect1[0][2];
-    int c = rect2[0][2];
-    int depth[3];
-    int p, q, r;
+    // Initialize depths
+    int depth[3] = {boundry[0][2], rect1[0][2], rect2[0][2]};
 
-    if (a < c) {
-        p = b;
-        depth[0] = a;
-    } else {
-        q = b;
-        depth[0] = c;
+    // Sorting depths in decreasing order (Bubble Sort)
+    for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            if (depth[i] < depth[j]) {
+                int temp = depth[i];
+                depth[i] = depth[j];
+                depth[j] = temp;
+            }
+        }
     }
 
-    if (p < q) {
-        r = q;
-        depth[1] = p;
-        depth[2] = r;
-    } else {
-        r = p;
-        depth[1] = q;
-        depth[2] = r;
-    }
-
+    // Filling topmost (visible) surface first using correct patterns
     for (int j = 0; j < 3; j++) {
-        printf("\t%d", depth[j]);
+        if (depth[j] == boundry[0][2]) {
+            setfillstyle(HATCH_FILL, WHITE); // Hatch fill
+            bar(boundry[0][0], boundry[0][1], boundry[3][0], boundry[3][1]);
+        } else if (depth[j] == rect1[0][2]) {
+            setfillstyle(LINE_FILL, WHITE); // Horizontal lines
+            bar(rect1[0][0], rect1[0][1], rect1[3][0], rect1[3][1]);
+        } else if (depth[j] == rect2[0][2]) {
+            setfillstyle(SLASH_FILL, WHITE); // Left-slash fill
+            bar(rect2[0][0], rect2[0][1], rect2[3][0], rect2[3][1]);
+        }
     }
 
-    if (depth[2] == a) {
-        setcolor(BLUE);
-        setfillstyle(SOLID_FILL, BLUE);
-        bar(boundry[0][0], boundry[0][1], boundry[3][0], boundry[3][1]);
-    } else if (depth[2] == b) {
-        setcolor(RED);
-        setfillstyle(SOLID_FILL, RED);
-        bar(rect1[0][0], rect1[0][1], rect1[3][0], rect1[3][1]);
-    } else if (depth[2] == c) {
-        setcolor(GREEN);
-        setfillstyle(SOLID_FILL, GREEN);
-        bar(rect2[0][0], rect2[0][1], rect2[3][0], rect2[3][1]);
-    }
-
+    delay(2000); // Hold window for visualization
     getchar();
     closegraph();
     return 0;
 }
-
